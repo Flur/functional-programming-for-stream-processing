afa46cf9db34064616104a622a2e5f729a62be8b
avedilin <superflur@gmail.com>
2021-01-09 21:08:10 +0200

    - draft version for disaster source

--- build.sbt
+++ build.sbt
@@ -13,5 +13,6 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % AkkaVersion,
   "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
   "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
-  "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion
+  "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
+//  "org.tpolecat" %% "doobie-postgres" % "0.5.3"
 )
--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -12,17 +12,8 @@ import scala.util.Try
 
 
 object Main extends App {
-  implicit val system = ActorSystem()
-  implicit val dispatcher = system.dispatcher
-  implicit val materializer = ActorMaterializer()
 
-  val httpRequest = HttpRequest(HttpMethods.GET, "https://eonet.sci.gsfc.nasa.gov/api/v3/events")
+//  NasaDisasterSource()
+  PostgresSink()
 
-  val source: Source[(HttpRequest, String), NotUsed] = Source.single((httpRequest, "test"))
-
-  val flow: Flow[(HttpRequest, String), (Try[HttpResponse], String), NotUsed] = Http().superPool[String]()
-
-  val sink: Sink[(Try[HttpResponse], String), Future[Done]] = Sink.foreach[(Try[HttpResponse], String)]((a: (Try[HttpResponse], String)) => println(a._1))
-
-  val mat = source.via(flow).runWith(sink)
 }
